90f1c0be56ac5b2e516be2366002cd2277308900
Evil <127085316+Zastt@users.noreply.github.com>
2025-04-06 21:14:50 +0300

    Update Class1.cs

--- labs/OOP/2.1/Class1.cs
+++ labs/OOP/2.1/Class1.cs
@@ -1,8 +1,55 @@
-﻿using System;
+using System;
 
-public class Class1
+class MyString
 {
-	public Class1()
-	{
-	}
+    protected string value;
+
+    public MyString(string val)
+    {
+        value = val;
+    }
+
+    public int GetLength()
+    {
+        return value.Length;
+    }
+
+    public virtual void Display()
+    {
+        Console.WriteLine("Рядок: " + value);
+    }
+}
+
+class CharString : MyString
+{
+    public CharString(string val) : base(val) {}
+
+    public string SortDescending()
+    {
+        char[] chars = value.ToCharArray();
+        Array.Sort(chars);
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+
+    public override void Display()
+    {
+        Console.WriteLine("Символьний рядок: " + value);
+        Console.WriteLine("Сортований за спаданням: " + SortDescending());
+        Console.WriteLine("Довжина: " + GetLength());
+    }
+
+    public string GetValue()
+    {
+        return value;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        CharString myStr = new CharString("Helllooooooo");
+        myStr.Display();
+    }
 }
